127e9dba0ffc6b9e656ce1dba3eac1c3c3caafa6
vbytex <vishalbaraiofficial01@gmail.com>
2026-03-19 20:05:37 +0530

    chore:Added cta in landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,11 +30,11 @@ export default function Home() {
               Explore
             </Button>
           </Link>
-          <Link href="/recursion-2026" className="w-full md:w-auto">
+          <Link href="/recursion-2026/shortlisted" className="w-full md:w-auto">
             <button className="relative w-full md:w-auto px-6 py-3 bg-[#8B0000] text-white font-bold uppercase tracking-widest border border-red-900 overflow-hidden group shadow-[0_0_15px_rgba(139,0,0,0.5)] transition-all hover:shadow-[0_0_25px_rgba(212,175,55,0.7)] hover:border-[#D4AF37] cursor-pointer">
               <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/20 to-transparent -translate-x-[150%] group-hover:animate-[shimmer_1.5s_infinite]" />
               <span className="relative z-10 flex items-center justify-center gap-2">
-                Checkout Recursion 7.0{" "}
+                Checkout Recursion 7.0 shortlisted teams{" "}
                 <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform" />
               </span>
 
